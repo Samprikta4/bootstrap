9ab0a0461274aa5b4c39b49ba6b53a4c2c1a140a
Samprikta4 <ssameersarker@gmail.com>
2025-07-24 16:18:04 +0600

    Add files via upload

---
+++ Story.js
@@ -0,0 +1,18 @@
+
+
+
+
+function typeandscream() {
+var personName= document.getElementById ("name").value;
+alert ("I told you not to do that" + personName);
+}
+function godark() {
+
+document.body.className="dark";
+
+}
+function finishthestory() {
+
+document.getElementById("story").innerHTML ="So i carefully searched the damp dungeon. There you are!"
+
+}
