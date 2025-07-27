93ab101e88cadae776c2187ad6d7d6763ed2a295
Samprikta4 <ssameersarker@gmail.com>
2025-07-27 17:17:36 +0600

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,19 @@
+let div = document.querySelector('.rectangle');
+function changeToCircle() {
+    div.style.borderRadius = "50%";
+}
+function hide() {
+    div.style.display = "none";
+}
+function show() {
+
+    div.style.display = "block";
+}
+div.onmouseenter = function() {
+
+    div.style.background = "red";
+}
+    
+div.onmouseout = function() {
+    div.style.background = "none";
+}
